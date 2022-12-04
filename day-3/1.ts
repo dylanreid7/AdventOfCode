ae1be47b49d8ec0635e62127656e732015d24583
Dylan <dylanreid7@gmail.com>
2022-12-04 16:13:29 -0600

    day 3 revised

--- day-3/1.ts
+++ day-3/1.ts
@@ -3,37 +3,30 @@ const input = fs.readFileSync('input.txt', 'utf8');
 
 const inputList = input.split('\n');
 
-const findCommonLetter = (strOne: string, strTwo: string) => {
-    for (let i = 0; i < strOne.length; i++) {
-        if (strTwo.includes(strOne[i])) {
-            return strOne[i];
+const findCommonNum = (one: number[], two: number[]): number => {
+    const secondSet = new Set(two);
+    for (let i = 0; i < one.length; i++) {
+        if (secondSet.has(one[i])) {
+            return one[i];
         }
-    }
-    return '';
+    };
+    return 0;
 }
 
 const determineLetterNumber = (str: string): number => {
-    let charCode = str.charCodeAt(0);
-    console.log('charCode og', charCode);
-    if (charCode >= 65 && charCode <= 90) {
-        // get to 27
-        charCode -= 38;
-    } else if (charCode >= 97 && charCode <= 122) {
-        charCode -= 96;
-    }
-    return charCode;
+    if (str === str.toUpperCase()) {
+        return str.charCodeAt(0) - 38;
+    } 
+    return str.charCodeAt(0) - 96;
 }
 
-let sum = 0;
+let sum: number = 0;
 inputList.forEach((ruckSack: string) => {
     const ruckLength = Math.floor(ruckSack.length / 2);
-    let ruckFirstHalf = ruckSack.slice(0, ruckLength);
-    let ruckSecondHalf = ruckSack.slice(ruckLength);
-    let commonLetter = findCommonLetter(ruckFirstHalf, ruckSecondHalf);
-    console.log('common letter: ', commonLetter);
-    let letterNumber = determineLetterNumber(commonLetter);
-    console.log('letter num: ', letterNumber);
-    sum += letterNumber;
+    let ruckFirstHalf = ruckSack.slice(0, ruckLength).split('').map((s) => determineLetterNumber(s));
+    let ruckSecondHalf = ruckSack.slice(ruckLength).split('').map((s) => determineLetterNumber(s));
+    let commonNum = findCommonNum(ruckFirstHalf, ruckSecondHalf);
+    sum += commonNum;
 })
 
 console.log('sum: ', sum);
